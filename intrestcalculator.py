ed999304d12d1edc9f223f2dc0826ee0a08f516a
nagaraju686 <nagarajunagmelam@gmail.com>
2026-06-13 08:59:33 +0530

    first msg

--- intrestcalculator.py
+++
@@ -1,5 +0,0 @@
-n1=int(input("Enter the principal amount:"))
-n2=float(input("Enter the rate of interest:"))
-n3=int(input("Enter the time token"))    
-si=(n1*n2*n3)/100
-print("The simple interest is:",si)    
